118823beea2f8787385fad5c392e8f4136f3702d
andrewaziz8 <andrewwassem@aucegypt.edu>
2024-07-06 18:37:51 +0300

    Last Version Demo UI

--- El_3af4a_2af4a/El_3af4a_2af4a/app/build.gradle.kts
+++ El_3af4a_2af4a/El_3af4a_2af4a/app/build.gradle.kts
@@ -41,4 +41,5 @@ dependencies {
     androidTestImplementation(libs.ext.junit)
     androidTestImplementation(libs.espresso.core)
     implementation ("com.intuit.sdp:sdp-android:1.1.1")
+    implementation ("com.google.android.material:material:1.6.0")
 }
